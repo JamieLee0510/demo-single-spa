d91f26e48ff3e2af5fc8961975a608d4c3610c3e
Jamie Lee <jamielee@onework.io>
2023-05-15 15:11:59 +0800

    UPDATE:react and vue single-spa project

---
+++ demo-project/vue-app/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/vue'), // 為了配合微前端路由
+  routes
+})
+
+export default router
